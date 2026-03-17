b052329132f28a3665a5bbee67eb18063a6f1314
Ostap309 <mr.ostap2005@yandex.ru>
2026-03-17 13:15:02 +0300

    Первая рабочая версия. Битовые маски.

--- Homework/lesson4-5/independent_set_problem.py
+++ Homework/lesson4-5/independent_set_problem.py
@@ -4,8 +4,9 @@ import numpy as np
 import time
 
 SEED = 42  # Случайное начальное число
-PROBABILITY = 0.3  # Вероятность появления ребра
-N_RANGE = range(5, 3000, 10)  # Количество вершин
+PROBABILITY = 0.1  # Вероятность появления ребра
+N_RANGE = range(5, 21)  # Количество вершин
+DRAW_AND_QUIT = -1 # Преждевременная остановка с визуализацией графа (-1 - откл.)
 RETESTS = 5  # Количество испытаний для каждого числа вершин
 
 times_mean = []  # среднее время выполнения
@@ -32,14 +33,60 @@ def measure_execution_time(func):
     return wrapper
 
 
-@measure_execution_time
-def algorithm(graph: nx.Graph):
-    return len(nx.maximal_independent_set(graph))
+class ConnectivityList:
+    def __init__(self, graph: nx.Graph):
+        self.conlist = []
+        self.length = graph.number_of_nodes()
+        # Для каждой вершины будем хранить маску связности в списке связности по индексу с номером этой вершины
+        for node in range(self.length):
+            self.conlist.append(self.to_binary(graph.neighbors(node)))
 
-    # pos = nx.spring_layout(graph)
-    # nx.draw(graph, pos, with_labels=True, node_color='lightblue', node_size=500)
-    # plt.title("Граф Эрдеша‑Реньи (seed=42)")
-    # plt.show()
+    @staticmethod
+    def to_binary(neighbors) -> int:
+        binary = 0
+
+        # Создаем маску связности
+        for i in neighbors:
+            binary |= 1 << i
+        return binary
+
+    def __str__(self) -> str:
+        res_str = ""
+        for node, mask in enumerate(self.conlist):
+            res_str += f"{node}: {bin(mask)}\n"
+
+        return res_str
+
+    def check_independence(self, subset: int) -> bool:
+        neighbors_mask = 0
+        temp = subset
+        index = 0
+        while temp:
+            if temp & 1:
+                neighbors_mask |= self.conlist[index]
+            temp >>= 1
+            index += 1
+
+        if not neighbors_mask & subset:
+            return True
+
+        return False
+
+
+@measure_execution_time
+def algorithm(graph: nx.Graph) -> int:
+    cl = ConnectivityList(graph)
+    misl = 0  # max independent set length (искомое значение)
+
+    cur_subset = 1
+    while cur_subset <= (1 << cl.length) - 1:
+        if cl.check_independence(cur_subset):
+            nodes_count = cur_subset.bit_count()
+            if nodes_count > misl:
+                misl = nodes_count
+        cur_subset += 1
+
+    return misl
 
 
 # Создание графов, запуск алгоритма, построение графика производительности
@@ -47,19 +94,32 @@ def main():
     global times_mean, times_std, times
 
     for n in N_RANGE:
-        new_graph = nx.erdos_renyi_graph(n, p=PROBABILITY, seed=SEED)
+        graph = nx.erdos_renyi_graph(n, p=PROBABILITY, seed=SEED)
+
+        # Объявляем новый порядок вершин по возрастанию степеней
+        node_order = [node for node, _ in sorted(dict(graph.degree()).items(), key=lambda x: x[1])]
+        # Сопоставляем каждой вершине её новое название
+        mapping = {old: new for new, old in enumerate(node_order)}
+        # переименовываем вершены, сохраняем полученную копию в новую переменную
+        sorted_graph = nx.relabel_nodes(graph, mapping)
 
         for _ in range(RETESTS):
-            algorithm(new_graph)
-            # quit()
+            result = algorithm(sorted_graph)
+
+            if DRAW_AND_QUIT != -1 and n >= DRAW_AND_QUIT:
+                print(result)
+                pos = nx.spring_layout(sorted_graph)
+                nx.draw(sorted_graph, pos, with_labels=True, node_color='lightblue', node_size=500)
+                plt.show()
+                quit()
 
         times_mean.append(times.mean())
         times_std.append(times.std())
 
-    print(times_mean)
-    print(times_std)
+    # print(times_mean)
+    # print(times_std)
 
-    plt.scatter(N_RANGE, times_mean)
+    plt.plot(N_RANGE, times_mean)
     plt.show()
 
 
