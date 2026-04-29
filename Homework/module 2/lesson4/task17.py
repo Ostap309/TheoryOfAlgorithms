865a0ae41a4f623a920ce474cab646830046fc45
Ostap309 <mr.ostap2005@yandex.ru>
2026-04-29 11:05:05 +0300

    Упражнение 17(2) Перебор

---
+++ Homework/module 2/lesson4/task17.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+
+
+def get_permutations_itertools(n):
+    numbers = list(range(n))
+    perms = [list(p) for p in permutations(numbers)]
+    return perms
+
+
+n = int(input())
+ri = list(map(float, input().split(", ")))
+best_result, best_order = 0, []
+for order in get_permutations_itertools(n):
+    result = 0
+    for t in range(n):
+        result += 100 * ri[order[t]] ** t
+        print(" +", 100 * ri[order[t]] ** t)
+    if result > best_result:
+        best_result = result
+        best_order = order
+
+    print(result, "|", " -> ".join(map(lambda x: str(ri[x]), order)))
+    print()
+
+print("=========\nBEST:")
+print(best_result, "|", " -> ".join(map(lambda x: str(ri[x]), best_order)))
